2b0953da4181069d0d49771caa3b23d460e361fc
Clark Ozanne <clarkozanne@gmail.com>
2016-01-28 07:34:14 -0800

    added Filter

--- public/javascripts/sort.js
+++ public/javascripts/sort.js
@@ -1,12 +1,22 @@
-// 'use strict';
-//
-// $(document).ready(initer);
-//
-// function initer(){
-//   $('.theC').on('click', sortbyCompany)
-// }
-//
-// function sortbyCompany(){
-//   $.get('/users/sortc')
-//   .done(function(data){})
-// }
+'use strict';
+
+$(document).ready(initer);
+
+function initer(){
+  $('#fraudLooker').on('change', fraudFilter)
+  $('.theC').on('click', sortbyCompany)
+
+}
+
+function sortbyCompany(){
+  $.get('/users/sortc')
+  .done(function(data){})
+}
+
+function fraudFilter(){
+  console.log("boom");
+  if ($('#fraudLooker').prop("checked")){
+    $(".false").hide()
+  }
+  else{ $(".false").show()}
+}
