6b2455b1c3a9a306e51ada4f21a5d852f5f32326
Clark Ozanne <clarkozanne@gmail.com>
2016-01-27 23:02:37 -0800

    Some Css. Work on sorting tomorrow mo

--- public/javascripts/sort.js
+++ public/javascripts/sort.js
@@ -1,11 +1,12 @@
-'use strict';
-
-$(document).ready(initer);
-
-function initer(){
-  $('.theC').on('click', sortbyCompany)
-}
-
-function sortbyCompany(){
-
-}
+// 'use strict';
+//
+// $(document).ready(initer);
+//
+// function initer(){
+//   $('.theC').on('click', sortbyCompany)
+// }
+//
+// function sortbyCompany(){
+//   $.get('/users/sortc')
+//   .done(function(data){})
+// }
